30952452ab59eba01033cc6427a9fe73bf90b478
z0s3r77 <ipopdue24@gmail.com>
2023-02-27 18:33:52 +0100

    feature(*)

---
+++ sneakerraffler/src/main/java/edu/craptocraft/sneakerraffle/App.java
@@ -0,0 +1,13 @@
+package edu.craptocraft.sneakerraffle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
